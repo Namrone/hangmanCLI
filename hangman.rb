12d8f82b7eeeae2506750f0aff7c7dea51f731aa
Namrone <151798569+Namrone@users.noreply.github.com>
2025-05-23 11:32:20 -0700

    Added setters -> word & correct_guesses, methods -> guess_check, end_game in hangman.rb. Also added method -> print_hangman in hangmanCLI

--- hangman.rb
+++ hangman.rb
@@ -1,23 +1,55 @@
 class Hangman
-attr_accessor :guesses, :word
+  attr_accessor :correct_guesses, :incorrect_guesses,:word
 
-def initialize(guesses, new_word = true)
-  @guesses = guesses
-  @new_word = new_word
-end
-
-def pick_word
-  word_list = Array.new
-  File.open("google-10000-english-no-swears.txt", "r") do |file|
-    file.each_line do |word|
-      word_list << word if word.length >= 5 && word.length <= 12
+  def initialize(correct_guesses, incorrect_guesses = [], word = false)
+    self.word = word
+    self.correct_guesses = correct_guesses
+    @incorrect_guesses = incorrect_guesses
   end
-  word_list.sample
-end
 
-def char_in_word(char)
-  word.include?(char)
-end
+  def word=(input) # <= sets the word to a random wordfrom word list or from saved file
+    if input
+    else
+      word_list = Array.new
+      File.open("google-10000-english-no-swears.txt", "r") do |file|
+        file.each_line do |word_sample|
+          word_list << word_sample.chomp if word_sample.length >= 5 && word_sample.length <= 12
+        end
+      end
+      @word = word_list.sample
+    end
+  end
 
+  def correct_guesses=(input) # <= sets correct guess length to either new word or the saved amount
+    if @correct_guesses == nil
+      @correct_guesses = Array.new(@word.length)
+    else
+      @correct_guesses = input
+    end
+  end
 
+  def guess_check(char) # <= checks if guess has been guessed already & valid then sets it into correct variable
+    char = char.downcase
+    if @correct_guesses.include?(char) || @incorrect_guesses.include?(char)
+      puts "You have already guessed '#{char}'. Please enter a different char"
+      return
+    end
+    if !char.match?(/[[:alpha:]]/) || char.length > 1
+      puts "Invalid: Please enter a letter"
+      return
+    end
+    if @word.include?(char) 
+      @correct_guesses[@word.index(char)] = char
+    else
+      @incorrect_guesses << char
+    end
+  end
+
+  def end_game
+     if @correct_guesses.to_s == @word
+       puts "You win!"
+     elsif @incorrect_guesses.length == 6
+        puts "You lose, the word was #{@word}"
+     end
+  end
 end
--- hangmanCLI.rb
+++ hangmanCLI.rb
@@ -1,2 +1,55 @@
-require "hangman.rb"
+require_relative "hangman"
 
+def print_hangman(wrong)
+  case wrong
+  when 0
+    puts "|‾‾‾‾‾|"
+    puts "|"
+    puts "|"
+    puts "|"
+    puts "|"
+    puts "|_____"
+  when 1
+    puts "|‾‾‾‾‾|"
+    puts "|     O"
+    puts "|"
+    puts "|"
+    puts "|"
+    puts "|_____"
+  when 2
+    puts "|‾‾‾‾‾|"
+    puts "|     O"
+    puts "|     |"
+    puts "|"
+    puts "|"
+    puts "|_____"
+  when 3
+    puts "|‾‾‾‾‾|"
+    puts "|     O"
+    puts "|    /|"
+    puts "|"
+    puts "|"
+    puts "|_____"
+  when 4
+    puts "|‾‾‾‾‾|"
+    puts "|     O"
+    puts "|    /|\\"
+    puts "|"
+    puts "|"
+    puts "|_____"
+  when 5
+    puts "|‾‾‾‾‾|"
+    puts "|     O"
+    puts "|    /|\\"
+    puts "|    /"
+    puts "|"
+    puts "|_____"
+  when 6
+    puts "|‾‾‾‾‾|"
+    puts "|     O"
+    puts "|    /|\\"
+    puts "|    / \\"
+    puts "|"
+    puts "|_____"
+  end
+end
